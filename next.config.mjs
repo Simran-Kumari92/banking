516003cd960b24b8376db75a2b9a62c125109f99
Simran-Kumari92 <simranku20@gmail.com>
2025-02-05 19:49:04 +0530

    implement initial file and folder structure

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// .ts
+
+
+
+
+
+
+// next.config.mjs
+export default {
+  reactStrictMode: true,
+  // Add other Next.js config options here
+};
+
+
+
+
+

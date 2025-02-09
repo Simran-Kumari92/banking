3c1cd3dceac987aa69fd4fa66e8566280695938b
Simran-Kumari92 <simranku20@gmail.com>
2025-02-09 19:43:16 +0530

    Implement auth, appwrite, and sentry

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -16,6 +16,7 @@ import {
 } from "../components/ui/sheet";
 import { sidebarLinks } from '../constants';
 import { usePathname } from 'next/navigation';
+import Footer from './Footer';
 
 
 const MobileNav = ({ user }: MobileNavProps) => {
@@ -79,7 +80,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
              </nav>
           </SheetClose>
 
-          FOOTER
+          <Footer user={user} type="mobile" />
         </div>
       </SheetContent>
     </Sheet>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://60cd22af1f4c6934a7317e7630a226d9@o4508788591230976.ingest.us.sentry.io/4508788595884032",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
